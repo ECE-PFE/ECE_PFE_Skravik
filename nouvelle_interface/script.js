38859ce82d1d881b2e67e3f3c8a6975956f9d9ac
NeilSCGH <neil.segard@gmail.com>
2021-01-06 15:16:13 +0100

    pages created

--- nouvelle_interface/script.js
+++ nouvelle_interface/script.js
@@ -1,14 +1,27 @@
-function panneauSolairePage() {
-  alert("panneau solaire cliqué");
+function PagePanneauxSolaires() {show('PagePanneauxSolaires');}
+function PageEoliennes() {show('PageEoliennes');}
+function PageHydroliennes() {show('PageHydroliennes');}
+function PageMoteur() {show('PageMoteur');}
+function PageEquipements() {show('PageEquipements');}
+
+function show(elementID) {
+   // try to find the requested page and alert if it's not found
+   var ele = document.getElementById(elementID);
+   if (!ele) {
+       alert("no such element");
+       return;
+   }
+
+   // get all pages, loop through them and hide them
+   var pages = document.getElementsByClassName('page');
+   for(var i = 0; i < pages.length; i++) {
+       pages[i].style.display = 'none';
+   }
+
+   // then show the requested page
+   ele.style.display = 'block';
 }
 
-function eoliennesPage() {
-  alert("eolienne cliquée");
-}
-
-function hydroliennesPage() {
-  alert("hydrolienne cliquée");
-}
 
 (function () {
   var updateValues = function () {
@@ -23,6 +36,8 @@ function hydroliennesPage() {
         document.getElementById("panneauxsolaires").innerHTML = "+" + obj.data.sources.panneauxSolaires.production + " kW";
         document.getElementById("eoliennes").innerHTML = "+" + obj.data.sources.eoliennes.production + " kW";
         document.getElementById("hydroliennes").innerHTML = "+" + obj.data.sources.hydroliennes.production + " kW";
+        document.getElementById("moteur").innerHTML = obj.data.consos.moteur.conso + " kW";
+        document.getElementById("equipements").innerHTML = obj.data.consos.equipements.conso + " kW";
         
       }
     };
