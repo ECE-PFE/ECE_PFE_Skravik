c07a267e9698d5f01d51c0b21d4cfc445a12a226
NeilSCGH <neil.segard@gmail.com>
2021-01-07 12:31:12 +0100

    detail added to panneaux solaires

--- interface/script.js
+++ interface/script.js
@@ -27,14 +27,23 @@ function show(elementID) {
         const json = xhr.responseText;
         const obj = JSON.parse(json);
 
-        document.getElementById("panneauxsolaires").innerHTML = "+" + obj.data.sources.panneauxSolaires.production + " kW";
-        document.getElementById("eoliennes").innerHTML = "+" + obj.data.sources.eoliennes.production + " kW";
-        document.getElementById("hydroliennes").innerHTML = "+" + obj.data.sources.hydroliennes.production + " kW";
-        document.getElementById("moteur").innerHTML = obj.data.consos.moteur.conso + " kW";
-        document.getElementById("equipements").innerHTML = obj.data.consos.equipements.conso + " kW";
-        document.getElementById("sommeSources").innerHTML = obj.data.sommes.sommeSources + " kW";
-        document.getElementById("sommeConsos").innerHTML = obj.data.sommes.sommeConsos + " kW";
-        document.getElementById("sommeTotale").innerHTML = obj.data.sommes.sommeTotale + " kW";
+        //Page menu
+        document.getElementById("panneauxsolaires").innerHTML = "+" + obj.data.sources.panneauxSolaires + " kW";
+        document.getElementById("eoliennes").innerHTML        = "+" + obj.data.sources.eoliennes + " kW";
+        document.getElementById("hydroliennes").innerHTML     = "+" + obj.data.sources.hydroliennes + " kW";
+        document.getElementById("groupeEletrogene").innerHTML = "+" + obj.data.sources.groupeElectrogene.production + " kW";
+        document.getElementById("alternateur").innerHTML      = "+" + obj.data.sources.alternateur.production + " kW";
+        document.getElementById("moteur").innerHTML           = obj.data.consos.moteur.conso + " kW";
+        document.getElementById("equipements").innerHTML      = obj.data.consos.equipements.conso + " kW";
+        document.getElementById("sommeSources").innerHTML     = obj.data.sommes.sommeSources + " kW";
+        document.getElementById("sommeConsos").innerHTML      = obj.data.sommes.sommeConsos + " kW";
+        document.getElementById("sommeTotale").innerHTML      = obj.data.sommes.sommeTotale + " kW";
+
+        //Page panneaux solaires
+        document.getElementById("PSProduction1").innerHTML     = "Production : " + obj.data.sources.panneauSolaire1.production + " kW";
+        document.getElementById("PSInclinaison1").innerHTML    = "Inclinaison : " + obj.data.sources.panneauSolaire1.inclinaison + " °";
+        document.getElementById("PSRendement1").innerHTML      = "Rendement : " + obj.data.sources.panneauSolaire1.rendement + " %";
+        document.getElementById("PSEnsoleillement1").innerHTML = "Ensoleillement : " + obj.data.sources.panneauSolaire1.ensoleillement + " %";
 
       }
     };
